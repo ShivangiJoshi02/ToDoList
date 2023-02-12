b2ada1820305f40c165d1ef4ecac26606722ed21
Shivangi Joshi <shivangijoshi153@gmail.com>
2023-02-12 14:59:40 +0530

    Updates

---
+++ precache-manifest.68f6d677ba54e49b94ee45d1e1634700.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef1e302cc81d9c5ff702",
+    "url": "/ToDoList/static/css/main.68c0d922.chunk.css"
+  },
+  {
+    "revision": "ef1e302cc81d9c5ff702",
+    "url": "/ToDoList/static/js/main.ef1e302c.chunk.js"
+  },
+  {
+    "revision": "e24788202112317f4c80",
+    "url": "/ToDoList/static/js/1.e2478820.chunk.js"
+  },
+  {
+    "revision": "b0420226dc1dc1a6d39c",
+    "url": "/ToDoList/static/js/runtime~main.b0420226.js"
+  },
+  {
+    "revision": "a085daf92458a2c0e3879a7016bb179f",
+    "url": "/ToDoList/index.html"
+  }
+];
